d38a997530e83607e0de5fdb42d2475e202ce0c0
Sean Genge <seangenge@gmail.com>
2022-08-07 13:44:38 +1000

    Updated port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const moment = require('moment');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
